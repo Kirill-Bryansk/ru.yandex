d8d185e40143997583394f45d600df8aa6a60c4a
Kirill-Bryansk <k.mashnov@yandex.ru>
2025-05-08 10:52:24 +0300

    Изменил структуру пакетов

---
+++ src/practicum/sprint_5/lesson_Abstraction/Task_2/Rectangle.java
@@ -0,0 +1,8 @@
+package practicum.sprint_5.lesson_Abstraction.Task_2;
+
+public class Rectangle extends Parallelogram  {
+
+    public Rectangle(double a, double b) {
+        super(a, b);
+    }
+}
---
+++ src/practicum/sprint_5/lesson_Abstraction/Task_2/Rhombus.java
@@ -0,0 +1,8 @@
+package practicum.sprint_5.lesson_Abstraction.Task_2;
+
+public class Rhombus extends Parallelogram {
+
+    public Rhombus(double a, double h) {
+        super(h, a); // ВОТ ТАКАЯ ХУйня
+    }
+}
