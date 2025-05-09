ec529ee788195802d45169a1bf20d698c7d6d3ce
Kirill-Bryansk <k.mashnov@yandex.ru>
2025-05-09 11:34:19 +0300

    Добавил занятия

---
+++ src/practicum/sprint_4/lesson_Inheritance/Task_2/ModelP.java
@@ -0,0 +1,10 @@
+package practicum.sprint_4.lesson_Inheritance.Task_2;
+
+class ModelP extends Transport {
+    public ModelP() {
+        speed = 0;
+        maxSpeed = 120;
+        acceleration = 10;
+        brakingSpeed = 50;
+    }
+}
---
+++ src/practicum/sprint_4/lesson_Inheritance/Task_2/ModelQ.java
@@ -0,0 +1,10 @@
+package practicum.sprint_4.lesson_Inheritance.Task_2;
+
+class ModelQ extends TexlaCar {
+    public ModelQ() {
+        speed = 0;
+        maxSpeed = 300;
+        acceleration = 100;
+        brakingSpeed = 120;
+    }
+}
