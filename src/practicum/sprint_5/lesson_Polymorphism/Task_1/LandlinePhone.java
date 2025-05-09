550d754334f0aa975f435f4765426b97fe1c12db
Kirill-Bryansk <k.mashnov@yandex.ru>
2025-05-09 09:21:53 +0300

    Добавил занятия

---
+++ src/practicum/sprint_5/lesson_Polymorphism/Task_1/LandlinePhone.java
@@ -0,0 +1,8 @@
+package practicum.sprint_5.lesson_Polymorphism.Task_1;
+
+// допишите реализацию класса LandlinePhone
+public class LandlinePhone extends Phone {
+    public LandlinePhone(String number) {
+        super(number);
+    }
+}
